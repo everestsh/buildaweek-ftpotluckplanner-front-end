7bc901ee0630743110993194a12bba3d73bd602f
Mingjun Geng <sooofeve@gmail.com>
2022-03-02 02:35:38 -0800

    Home work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Home from './components/Home';
 import { Button } from 'reactstrap';
 
 function App() {
@@ -11,8 +11,11 @@ function App() {
     <div className="App">
 
       <Header />
-      <hr/>
-      <hr/>
+      {/* <hr/> */}
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      {/* <hr/> */}
       <Footer/>
       <h1>Front End</h1>
       <Button color="danger">Danger!</Button>
